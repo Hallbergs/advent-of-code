802758c199fc44590d68d7e0073a6df7671042c0
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2021-12-05 10:18:05 +0100

    Day 05 part 02 done. Also somewhat cleaned up solution for part 1

--- 2021/05/solution.js
+++ 2021/05/solution.js
@@ -21,51 +21,11 @@ export default class Solver {
     return x0 === x1 ? "VERTICAL" : y0 === y1 ? "HORIZONTAL" : "DIAGONAL";
   };
 
-  solveProblemOne = () => {
-    // We're gonna need a set to keep track of all occurrences
-    const occurrences = new Map();
-    // Then we'll set the occurring points for all lines
-    INPUT.map((line) => {
-      // We're gonna need the line coordinates...
-      const { x0, y0, x1, y1 } = this.#getLineCoordinates(line);
-      // ...and the line-type, to make sure we're not dealing
-      // with a diagonal line.
-      const lineType = this.#getLineType({ x0, y0, x1, y1 });
-      // We're only interested in vertical and horizontal lines
-      if (["VERTICAL", "HORIZONTAL"].includes(lineType)) {
-        // To draw all points between the start- and end-point,
-        // we get the number of points between the start and end.
-        const diff = lineType === "VERTICAL" ? y1 - y0 : x1 - x0;
-        // Check if we're dealing with a line going in a "positive" direction
-        // or not.
-        if (diff > 0) {
-          // If it is going in a positive direction, we increment x (if we are
-          // dealing with a horizontal line) or y (if we are dealing with a
-          // vertical line). Then we set the value in the map (either 1 or +1)
-          for (let i = 0; i <= diff; i++) {
-            const point = `${lineType === "VERTICAL" ? x0 : x0 + i},${
-              lineType === "VERTICAL" ? y0 + i : y0
-            }`;
-            const currentCount = occurrences.get(point);
-            occurrences.set(point, currentCount ? currentCount + 1 : 1);
-          }
-        } else {
-          // If it is going in a negative direction, we decrement x (if we are
-          // dealing with a horizontal line) or y (if we are dealing with a
-          // vertical line). Then we set the value in the map (either 1 or +1)
-          for (let j = 0; j <= Math.abs(diff); j++) {
-            const point = `${lineType === "VERTICAL" ? x0 : x0 - j},${
-              lineType === "VERTICAL" ? y0 - j : y0
-            }`;
-            const currentCount = occurrences.get(point);
-            occurrences.set(point, currentCount ? currentCount + 1 : 1);
-          }
-        }
-      }
-    });
-    // We want to return the number of overlaps, let's count them and return
+  // Returns the number of times a point is overlapped
+  // by two or more lines
+  #getNumOverlaps = (occurrenceMap) => {
     let numOverlaps = 0;
-    for (const value of occurrences.values()) {
+    for (const value of occurrenceMap.values()) {
       if (value >= 2) {
         numOverlaps++;
       }
@@ -73,17 +33,95 @@ export default class Solver {
     return numOverlaps;
   };
 
+  // Returns a map containing all points and how many times each
+  // point is "touched" by a line.
+  #getOccurrenceMap = (settings) => {
+    // Since we're not including diagonals in p1, we must provide
+    // a way to ignore the diagonals.
+    const includeDiagonals = settings.includeDiagonals ?? false;
+    // Let's initiate a map that can keep track of all points
+    const occurrences = new Map();
+    // Then we'll loop over all the lines and update the map
+    INPUT.map((line) => {
+      // We're gonna need the line coordinates...
+      const { x0, y0, x1, y1 } = this.#getLineCoordinates(line);
+      // ...and the line-type, to make sure we're not dealing
+      // with a diagonal line.
+      const lineType = this.#getLineType({ x0, y0, x1, y1 });
+      // We're handling the lineTypes a bit different, let's switch
+      // between them!
+      switch (lineType) {
+        case "VERTICAL":
+          // Let's start by calculating the vertical diff.
+          const vDiff = y1 - y0;
+          // Then we'll create new points that can be added (or updated)
+          // in the map. The points are created by incrementing (or decrementing)
+          // the starting-point all the way to the final point.
+          for (let i = 0; i <= Math.abs(vDiff); i++) {
+            const point = `${x0},${vDiff > 0 ? y0 + i : y0 - i}`;
+            const currentCount = occurrences.get(point);
+            occurrences.set(point, currentCount ? currentCount + 1 : 1);
+          }
+          break;
+        case "HORIZONTAL":
+          // Let's start by calculating the horizontal diff.
+          const hDiff = x1 - x0;
+          // Then we'll create new points that can be added (or updated)
+          // in the map. The points are created by incrementing (or decrementing)
+          // the starting-point all the way to the final point.
+          for (let j = 0; j <= Math.abs(hDiff); j++) {
+            const point = `${hDiff > 0 ? x0 + j : x0 - j},${y0}`;
+            const currentCount = occurrences.get(point);
+            occurrences.set(point, currentCount ? currentCount + 1 : 1);
+          }
+          break;
+        case "DIAGONAL":
+          // If we're not supposed to be dealing with diagonals, we abort.
+          if (!includeDiagonals) break;
+          // Otherwise, let's calculate some diffs
+          const diffX = x1 - x0;
+          const diffY = y1 - y0;
+          // Then we'll create new points that can be added (or updated)
+          // in the map. The points are created by incrementing (or decrementing)
+          // the starting-point all the way to the final point.
+          for (let k = 0; k <= Math.abs(diffX); k++) {
+            const point = `${diffX > 0 ? x0 + k : x0 - k},${
+              diffY > 0 ? y0 + k : y0 - k
+            }`;
+            const currentCount = occurrences.get(point);
+            occurrences.set(point, currentCount ? currentCount + 1 : 1);
+          }
+          break;
+        default:
+          break;
+      }
+    });
+    return occurrences;
+  };
+
+  solveProblemOne = () => {
+    // Let's get a map with all occurrences (not counting diagonal lines)
+    const occurrences = this.#getOccurrenceMap({ includeDiagonals: false });
+    // We want to return the number of overlaps, let's get them and return
+    return this.#getNumOverlaps(occurrences);
+  };
+
   solveProblemTwo = () => {
-    return null;
+    // Let's get a map with all occurrences (also counting diagonal lines)
+    const occurrences = this.#getOccurrenceMap({ includeDiagonals: true });
+    // We want to return the number of overlaps, let's get them and return
+    return this.#getNumOverlaps(occurrences);
   };
 }
 // Initiate the class
 const solver = new Solver();
 // And run the solvers
-console.time("solver");
+console.time("P1");
 const answerOne = solver.solveProblemOne();
+console.timeEnd("P1");
+console.time("P2");
 const answerTwo = solver.solveProblemTwo();
-console.timeEnd("solver");
+console.timeEnd("P2");
 
 console.log(
   `Answer for problem 01: ${answerOne}. Answer for problem 02: ${answerTwo}`
