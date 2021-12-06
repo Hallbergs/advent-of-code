79ca4a2a015d5b3f9230e8ff923654a74a6fc849
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2021-12-06 09:31:34 +0100

    Day 06 part 02 done. Complete rewrite of part 01 as well.

--- 2021/06/solution.js
+++ 2021/06/solution.js
@@ -9,23 +9,71 @@ export default class Solver {
     this.#initialTimer = 6;
   }
 
+  // Initiates a Map holding the amount of fish
+  // for each "timer". E.g. {0: 20, 1: 14,... 8: 13}
+  #initFishCounter = () => {
+    // Initialize a new Map()
+    const fishCounter = new Map();
+    // Let's iterate trough all fish, and add them to their
+    // place in the map.
+    for (const fish of INPUT) {
+      // If the fish (timer) does not exist, we add the first one
+      if (!fishCounter.has(fish)) {
+        fishCounter.set(fish, 1);
+      } else {
+        // Otherwise, we increment by one
+        fishCounter.set(fish, fishCounter.get(fish) + 1);
+      }
+    }
+    return fishCounter;
+  };
+
+  // Returns the total number of fish by iterating
+  // over all "timers" in the map and combining them.
+  #getTotalFishFromCounter = (fishCounter) => {
+    let numFish = 0;
+    for (let i = 0; i < 9; i++) {
+      numFish += fishCounter.get(i);
+    }
+    return numFish;
+  };
+
+  // Returns the number of fish present after `numDays` days.
   #getNumberOfFish = (numDays) => {
-    let fish = [...INPUT];
+    // First we have to init the fish counter with the
+    // initial amount of fish.
+    let fishCounter = this.#initFishCounter();
+    // Then we'll iterate the days
     for (let i = 0; i < numDays; i++) {
-      const newFish = [];
-      for (let ii = 0; ii < fish.length; ii++) {
-        if (fish[ii] !== 0) {
-          fish[ii] = fish[ii] - 1;
+      // We're gonna need a new map so that we can store the
+      // updated "timers" (fish).
+      const newFishCounter = new Map();
+      // Let's get the current amount of fish for each timer
+      for (const [key, value] of fishCounter.entries()) {
+        // If the timer is not zero (meaning we are not adding a new
+        // fish, we simply "move" those fish "down" one step). E.g. the
+        // amount of fish with timer 5 will be moved to timer 4.
+        if (key > 0) {
+          newFishCounter.set(
+            key - 1,
+            (newFishCounter.get(key - 1) ?? 0) + value
+          );
         } else {
-          newFish.push(this.#newBornTimer);
-          fish[ii] = this.#initialTimer;
+          // If we are dealing with a zero, we add a new fish with timer 8, and set
+          // all zero-timers to 6.
+          newFishCounter.set(
+            this.#newBornTimer,
+            (newFishCounter.get(this.#newBornTimer) ?? 0) + fishCounter.get(0)
+          );
+          newFishCounter.set(
+            this.#initialTimer,
+            (newFishCounter.get(this.#initialTimer) ?? 0) + fishCounter.get(0)
+          );
         }
       }
-      newFish.forEach((f) => {
-        fish.push(f);
-      });
+      fishCounter = newFishCounter;
     }
-    return fish.length;
+    return this.#getTotalFishFromCounter(fishCounter);
   };
 
   solveProblemOne = () => {
@@ -33,7 +81,7 @@ export default class Solver {
   };
 
   solveProblemTwo = () => {
-    return this.#getNumberOfFish(80);
+    return this.#getNumberOfFish(256);
   };
 }
 // Initiate the class
