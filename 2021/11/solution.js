d7330a3c8157861f585cb5997c859b2f5aa441c3
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2021-12-11 09:02:37 +0100

    Day 11 part 1 - some progress

--- 2021/11/solution.js
+++ 2021/11/solution.js
@@ -1,10 +1,82 @@
 import { INPUT } from "./input.js";
 
 export default class Solver {
-  constructor() {}
+  #rowLength;
+  #totalFlashes;
+
+  constructor() {
+    this.#rowLength = INPUT[0].length;
+    this.#totalFlashes = 0;
+  }
+
+  #getAdjacentIndexes = (index) => {
+    return index % this.#rowLength === 0
+      ? [
+          index + 1,
+          index - this.#rowLength,
+          index + this.#rowLength,
+          index - this.#rowLength + 1,
+          index + this.#rowLength + 1,
+        ]
+      : (index + 1) % this.#rowLength === 0
+      ? [
+          index - 1,
+          index - this.#rowLength,
+          index + this.#rowLength,
+          index - this.#rowLength - 1,
+          index + this.#rowLength - 1,
+        ]
+      : [
+          index - 1,
+          index + 1,
+          index - this.#rowLength,
+          index + this.#rowLength,
+          index - this.#rowLength - 1,
+          index + this.#rowLength - 1,
+          index - this.#rowLength + 1,
+          index + this.#rowLength + 1,
+        ];
+  };
+
+  #handleStep = (energyLevels) => {
+    const hasFlashed = new Set();
+    for (let i = 0; i < energyLevels.length; i++) {
+      energyLevels[i]++;
+      if (energyLevels[i] > 9) {
+        this.#handleFlash(energyLevels, i, hasFlashed);
+      }
+    }
+  };
+
+  #handleFlash = (energyLevels, flashIndex, hasFlashed) => {
+    if (hasFlashed.has(flashIndex)) {
+      return;
+    }
+    hasFlashed.add(flashIndex);
+    this.#totalFlashes++;
+    energyLevels[flashIndex] = 0;
+    const adjacentIndexes = this.#getAdjacentIndexes(flashIndex);
+    adjacentIndexes.forEach((index) => {
+      if (index < 0 || index > energyLevels.length) return;
+      energyLevels[index]++;
+      if (energyLevels[index] > 9) {
+        this.#handleFlash(energyLevels, index, hasFlashed);
+      }
+    });
+  };
 
   solveProblemOne = () => {
-    console.log(INPUT);
+    const energyLevels = [...INPUT]
+      .join("")
+      .split("")
+      .map((string) => parseInt(string));
+    console.log("before: ", energyLevels);
+    const numSteps = 100;
+    for (let i = 0; i < numSteps; i++) {
+      this.#handleStep(energyLevels);
+    }
+    console.log(energyLevels);
+    console.log("numFlashes: ", this.#totalFlashes);
   };
 
   solveProblemTwo = () => {};
