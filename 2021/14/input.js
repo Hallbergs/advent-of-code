d6b3b546e9cf51a1f1186d805762e024c43141e6
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2021-12-14 07:10:23 +0100

    Day 14 part 1 done

--- 2021/14/input.js
+++ 2021/14/input.js
@@ -7,4 +7,18 @@ export const INPUT = fs
   .readFileSync(path.join(__dirname, "input.txt"), "utf8")
   .toString()
   .trim()
-  .split("\r\n");
+  .split("\r\n")
+  .reduce(
+    (acc, line) => {
+      if (line === "") {
+        return acc;
+      }
+      if (line.includes("->")) {
+        acc[1].push(line);
+      } else {
+        acc[0] = acc[0].concat(line);
+      }
+      return acc;
+    },
+    ["", []]
+  );
--- 2021/14/solution.js
+++ 2021/14/solution.js
@@ -3,8 +3,50 @@ import { INPUT } from "./input.js";
 export default class Solver {
   constructor() {}
 
+  #initiateInsertionMap = (insertionRules) => {
+    const insertionMap = new Map();
+    insertionRules.forEach((rule) => {
+      const [key, value] = rule.split(" -> ");
+      insertionMap.set(key, value);
+    });
+    return insertionMap;
+  };
+
+  #getMostMinusLeastCommonCharacter = (templateArray) => {
+    const counter = new Map();
+    templateArray.forEach((character) => {
+      counter.set(character, (counter.get(character) ?? 0) + 1);
+    });
+
+    const sortedCounter = new Map(
+      [...counter.entries()].sort((a, b) => b[1] - a[1])
+    );
+    const sortedArray = [...sortedCounter.values()];
+    return sortedArray[0] - sortedArray[sortedArray.length - 1];
+  };
+
+  #step = (templateArray, insertionMap) => {
+    const originalArray = [...templateArray];
+    let numAdded = 0;
+    for (let i = 0; i < originalArray.length - 1; i++) {
+      const pair = `${originalArray[i]}${originalArray[i + 1]}`;
+      if (insertionMap.has(pair)) {
+        templateArray.splice(i + 1 + numAdded, 0, insertionMap.get(pair));
+        numAdded++;
+      }
+    }
+  };
+
   solveProblemOne = () => {
-    console.log(INPUT);
+    const templateArray = [...INPUT[0]];
+    const insertionRules = [...INPUT[1]];
+
+    const insertionMap = this.#initiateInsertionMap(insertionRules);
+
+    for (let i = 0; i < 10; i++) {
+      this.#step(templateArray, insertionMap);
+    }
+    return this.#getMostMinusLeastCommonCharacter(templateArray);
   };
 
   solveProblemTwo = () => {};
