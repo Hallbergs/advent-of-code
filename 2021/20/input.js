947c7967ef513313a851999c9cad9b46be2171e4
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2021-12-20 08:22:23 +0100

    Some progress on day 20

--- 2021/20/input.js
+++ 2021/20/input.js
@@ -4,7 +4,10 @@ import fs from "fs";
 const __dirname = path.resolve();
 
 export const INPUT = fs
-  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .readFileSync(path.join(__dirname, "input2.txt"), "utf8")
   .toString()
   .trim()
-  .split("\r\n");
+  .split("\r\n")
+  .filter((part) => {
+    return part !== "";
+  });
--- 2021/20/solution.js
+++ 2021/20/solution.js
@@ -3,8 +3,81 @@ import { INPUT } from "./input.js";
 export default class Solver {
   constructor() {}
 
+  #getBinaryFromIndex = (image, point) => {
+    const [xIndex, yIndex] = point;
+    const value = `${image[yIndex - 1]?.[xIndex - 1] ?? "."}${
+      image[yIndex - 1]?.[xIndex] ?? "."
+    }${image[yIndex - 1]?.[xIndex + 1] ?? "."}${
+      image[yIndex]?.[xIndex - 1] ?? "."
+    }${image[yIndex]?.[xIndex] ?? "."}${image[yIndex]?.[xIndex + 1] ?? "."}${
+      image[yIndex + 1]?.[xIndex - 1] ?? "."
+    }${image[yIndex + 1]?.[xIndex] ?? "."}${
+      image[yIndex + 1]?.[xIndex + 1] ?? "."
+    }`;
+    return value.split("").reduce((acc, char) => {
+      if (char === ".") {
+        acc += "0";
+      } else {
+        acc += "1";
+      }
+      return acc;
+    }, "");
+  };
+
+  #getTransformedValue = (transformation, binaryString) => {
+    const index = parseInt(binaryString, 2);
+    return transformation[index];
+  };
+
+  #getNewValue = (image, transformation, point) => {
+    const binaryString = this.#getBinaryFromIndex(image, point);
+    console.log(binaryString);
+    const transformedValue = this.#getTransformedValue(
+      transformation,
+      binaryString
+    );
+    return transformedValue;
+  };
+
+  #transformImage = (image, transformation) => {
+    const newImage = Array(image.length).fill(Array(image[0].length).fill("."));
+    for (let x = 0; x < image[0].length; x++) {
+      for (let y = 0; y < image.length; y++) {
+        newImage[y][x] = this.#getNewValue(image, transformation, [x, y]);
+      }
+    }
+    return newImage;
+  };
+
+  #getNumLitChars = (image) => {
+    let numLit = 0;
+    for (let x = 0; x < image[0].length; x++) {
+      for (let y = 0; y < image.length; y++) {
+        if (image[y][x] === "#") {
+          numLit++;
+        }
+      }
+    }
+    return numLit;
+  };
+
+  #padImage = (image) => {
+    return [
+      Array(image[0].length + 2).fill("."),
+      ...image.map((row) => [".", ...row, "."]),
+      Array(image[0].length + 2).fill("."),
+    ];
+  };
+
   solveProblemOne = () => {
-    console.log(INPUT);
+    const transformation = [...INPUT[0]];
+    let image = INPUT.splice(1);
+    image = this.#padImage(image);
+    image = this.#transformImage(image, transformation);
+    image = this.#padImage(image);
+    image = this.#transformImage(image, transformation);
+
+    return this.#getNumLitChars(image);
   };
 
   solveProblemTwo = () => {};
