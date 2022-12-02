df491631c090082f714823380cfd6e94d2b11217
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2022-12-02 07:37:40 +0100

    Day 02 part 1 done

--- 2022/02/solution.js
+++ 2022/02/solution.js
@@ -1,7 +1,19 @@
 import { INPUT } from "./input.js";
 
 export default class Solver {
-  solveProblemOne = () => {};
+  solveProblemOne = () => {
+    const moves = { p: ["X", "Y", "Z"], o: ["A", "B", "C"] };
+    const mb = new Map([
+      ["X", "C"],
+      ["Y", "A"],
+      ["Z", "B"],
+    ]);
+    return INPUT.reduce((a, c) => {
+      const [o, p] = c.split(" ");
+      const mi = moves.p.indexOf(p);
+      return a + (mb.get(p) === o ? 6 : o === moves.o[mi] ? 3 : 0) + mi + 1;
+    }, 0);
+  };
 
   solveProblemTwo = () => {};
 }
