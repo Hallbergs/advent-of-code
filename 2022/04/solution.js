679b5b35c2780e2f3537acb1238633541bc1ee29
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2022-12-06 12:52:44 +0100

    Day 04 done

--- 2022/04/solution.js
+++ 2022/04/solution.js
@@ -1,9 +1,35 @@
 import { INPUT } from "./input.js";
 
 export default class Solver {
-  solveProblemOne = () => {};
+  solveProblemOne = () => {
+    return [...INPUT].reduce((a, c) => {
+      const [s1, s2] = c.split(",");
+      const [a11, a12] = s1.split("-");
+      const [a21, a22] = s2.split("-");
+      if (
+        (parseInt(a11) <= parseInt(a21) && parseInt(a12) >= parseInt(a22)) ||
+        (parseInt(a21) <= parseInt(a11) && parseInt(a22) >= parseInt(a12))
+      ) {
+        a++;
+      }
+      return a;
+    }, 0);
+  };
 
-  solveProblemTwo = () => {};
+  solveProblemTwo = () => {
+    return [...INPUT].reduce((a, c) => {
+      const [s1, s2] = c.split(",");
+      const [a11, a12] = s1.split("-");
+      const [a21, a22] = s2.split("-");
+      if (
+        (parseInt(a11) <= parseInt(a21) && parseInt(a12) >= parseInt(a21)) ||
+        (parseInt(a21) <= parseInt(a11) && parseInt(a22) >= parseInt(a11))
+      ) {
+        a++;
+      }
+      return a;
+    }, 0);
+  };
 }
 
 const solver = new Solver();
