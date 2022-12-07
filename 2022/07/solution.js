3e8baef4e95f97f8222fca452977d926886d5b76
Henrik Hallberg <43059093+Hallbergs@users.noreply.github.com>
2022-12-07 12:10:54 +0100

    Day 07 done

--- 2022/07/solution.js
+++ 2022/07/solution.js
@@ -1,9 +1,52 @@
 import { INPUT } from "./input.js";
 
 export default class Solver {
-  solveProblemOne = () => {};
+  #getFileNameFromHistory = (history) => {
+    return history.reduce((a, c) => (a = `${a}-${c}`));
+  };
 
-  solveProblemTwo = () => {};
+  #getFileSystemStack = () => {
+    const history = [];
+    const stack = new Map();
+    INPUT.forEach((c) => {
+      const cc = c.replace("$ ", "");
+      const [o1, o2] = cc.split(" ");
+      if (o1 === "cd") {
+        if (o2 === "..") {
+          history.pop();
+        } else {
+          history.push(o2);
+          const fn = this.#getFileNameFromHistory(history);
+          !stack.has(fn) && stack.set(fn, 0);
+        }
+      } else if (!isNaN(parseInt(o1))) {
+        for (let i = 1; i <= history.length; i++) {
+          const fn = this.#getFileNameFromHistory(history.slice(0, i));
+          stack.set(fn, stack.get(fn) + parseInt(o1));
+        }
+      }
+    });
+    return stack;
+  };
+
+  solveProblemOne = () => {
+    return [...this.#getFileSystemStack().values()].reduce((a, c) => {
+      if (c <= 100000) {
+        a = a + c;
+      }
+      return a;
+    }, 0);
+  };
+
+  solveProblemTwo = () => {
+    const used = [...this.#getFileSystemStack().values()].sort(
+      (a, b) => b - a
+    )[0];
+    const toRemove = Math.abs(70000000 - 30000000 - used);
+    return [...this.#getFileSystemStack().values()]
+      .sort((a, b) => a - b)
+      .find((v) => v >= toRemove);
+  };
 }
 
 const solver = new Solver();
